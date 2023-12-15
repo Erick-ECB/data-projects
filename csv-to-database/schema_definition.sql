6617a36ebd758d425bf83a8ef408795286e3a8fb
Erick-ECB <erickecb123@gmail.com>
2023-12-14 20:46:50 -0600

    Modify schema_definition.sql

--- csv-to-database/schema_definition.sql
+++ csv-to-database/schema_definition.sql
@@ -1,16 +1,19 @@
 CREATE DATABASE book_catalog;
 
+
 CREATE TABLE books (
-  isbn13 CHAR(13) PRIMARY KEY,
-  isbn10 CHAR(10),
-  title VARCHAR(255),
-  subtitle VARCHAR(255),
-  authors TEXT[],
-  categories TEXT[],
-  thumbnail VARCHAR(150),
-  description TEXT,
-  published_year INTEGER,
-  average_rating NUMERIC,
-  num_pages INTEGER,
-  ratings_count INTEGER
+    isbn13 CHAR(13) PRIMARY KEY,
+    isbn10 CHAR(10),
+    title VARCHAR(255),
+    subtitle VARCHAR(255),
+    authors TEXT [],
+    categories TEXT [],
+    thumbnail VARCHAR(150),
+    description TEXT,
+    published_year INTEGER,
+    average_rating NUMERIC,
+    num_pages INTEGER,
+    ratings_count INTEGER
 );
+
+
